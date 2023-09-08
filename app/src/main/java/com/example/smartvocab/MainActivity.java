54ee14f8e1981fa921a7e4eed6ac6ca7ac84cb9a
MukeshKisku <km3660858@gmail.com>
2023-09-09 05:04:47 +0530

    Smart Vocabulary App

---
+++ app/src/main/java/com/example/smartvocab/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.smartvocab;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.OpenDrawer, R.string.CloseDrawer);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id==R.id.optHistory) {
+                    loadFragment(new AFragment());
+                } else if (id==R.id.optStarred) {
+                    loadFragment(new BFragment());
+                } else if (id==R.id.optBuy) {
+                    loadFragment(new CFragment());
+                }else if (id==R.id.optHelp) {
+                    loadFragment(new DFragment());
+                }else if (id==R.id.optAbout) {
+                    loadFragment(new EFragment());
+                }else if (id==R.id.optLookup) {
+                    loadFragment(new FFragment());
+                }else if (id==R.id.optSettings) {
+                    loadFragment(new GFragment());
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.container, fragment);
+        ft.commit();
+
+    }
+}
